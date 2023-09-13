80259b88ba7d79ad5db5715453a491e2ca1b06ce
unknown <vlfmap@gmail.com>
2023-09-13 07:36:47 +0200

    added map handler

--- webserver/endpoints/duegev/map-handler.endpoint.js
+++ webserver/endpoints/duegev/map-handler.endpoint.js
@@ -10,7 +10,64 @@ export class MapHandlerEndpointService {
             let query = req.body.query;
             let values = req.body.values;
 
-           
+            const GENERAL_INVALID_RESPONSE = () => {
+                res.set('content-type', 'text/plain');
+                res.send(JSON.stringify({ queryValidation: 'invalid' }));
+            }
+
+            switch (query) {
+                /*
+                {
+                    dates: {
+                        irl_date: string,
+                        date: string
+                    },
+                    updater_uid: number,
+                    description: string,
+                    map: {
+                        mapid: string,
+                        old_mapid: string,
+                        file: string
+                    }
+                }
+                */
+                case 'set-map':
+                    let update_map = `UPDATE map` +
+                        `SET date='${values.dates.date}', irl_date='${values.dates.irl_date}, updater_uid='${values.updater_uid}', description='${values.description}', mapid='${values.map.mapid}', file='${values.map.file}' ` +
+                        `WHERE mapid='${values.map.old_mapid}';`;
+
+                    genericQueryExecutor(update_map).then(dbResponse => {
+                        genericQueryExecutor(`SELECT * FROM map`).then(dbResponse => {
+                            if (dbResponse.queryValidation === 'valid') {
+                                const VALID_RESPONSE = {
+                                    queryValidation: 'valid',
+                                    values: 'successful-map-update'
+                                }
+                                res.set('content-type', 'text/plain');
+                                res.send(JSON.stringify(VALID_RESPONSE));
+                            } else {
+                                GENERAL_INVALID_RESPONSE();
+                            }
+                        });
+                    });
+                    break;
+
+                case 'get-map':
+                    genericQueryExecutor(`SELECT * FROM map`).then(dbResponse => {
+                        if (dbResponse.queryValidation === 'valid') {
+                            const VALID_RESPONSE = {
+                                queryValidation: 'valid',
+                                values: JSON.parse(JSON.stringify(dbResponse.values[0]))
+                            }
+                            res.set('content-type', 'text/plain');
+                            res.send(JSON.stringify(VALID_RESPONSE));
+                        } else {
+                            GENERAL_INVALID_RESPONSE();
+                        }
+                    });
+                    break;
+            }
+
         });
     }
 }
